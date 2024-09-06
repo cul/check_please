7bfa349b967cea24e84349584cfca04b6afb0c12
Eric O <eric@ejdigitalmedia.com>
2024-09-06 11:48:12 -0400

    Update dev.rb deployment target

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
-server 'fixity-test-2.svc.cul.columbia.edu', user: fetch(:remote_user), roles: %w[app db web]
+# server 'fixity-test-1.svc.cul.columbia.edu', user: fetch(:remote_user), roles: %w[app db web]
+# server 'fixity-test-2.svc.cul.columbia.edu', user: fetch(:remote_user), roles: %w[app db web]
+server 'fixity-test-3.svc.cul.columbia.edu', user: fetch(:remote_user), roles: %w[app db web]
 # Current branch is suggested by default in development
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
