0e9a702b8e28805fdab8008be6dbec75afb890da
Eric O <eric@ejdigitalmedia.com>
2026-05-15 18:19:50 -0400

    Update deploy targets; Update .gitignore

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-# server 'fixity-test-1.svc.cul.columbia.edu', user: fetch(:remote_user), roles: %w[app db web]
-# server 'fixity-test-2.svc.cul.columbia.edu', user: fetch(:remote_user), roles: %w[app db web]
-server 'fixity-test-3.svc.cul.columbia.edu', user: fetch(:remote_user), roles: %w[app db web]
+# server 'fixity-dev-1.svc.cul.columbia.edu', user: fetch(:remote_user), roles: %w[app db web]
+server 'not-currently-available.svc.cul.columbia.edu', user: fetch(:remote_user), roles: %w[app db web]
 # Current branch is suggested by default in development
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
--- config/deploy/test.rb
+++ config/deploy/test.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
-server 'not-available-yet.library.columbia.edu', user: fetch(:remote_user), roles: %w[app db web]
+# server 'fixity-test-1.svc.cul.columbia.edu', user: fetch(:remote_user), roles: %w[app db web]
+server 'not-currently-available.svc.cul.columbia.edu', user: fetch(:remote_user), roles: %w[app db web]
 # In test/prod, suggest latest tag as default version to deploy
 ask :branch, proc { `git tag --sort=version:refname`.split("\n").last }
